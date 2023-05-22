5fafb20655bac706c821272b80ad2b9a916159f3
Kiril Daskalov <webmatrixxxl@gmail.com>
2023-05-22 17:53:52 +0300

    feat: [wip] add React rehydratation

---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+// import { hydrateRoot } from "react-dom/client";
+// import React from "react";
+// import Home from "../../views/jsx/Components/Home.jsx";
+
+// hydrateRoot(
+//   document.getElementById("root"),
+//   <>
+//     <Home />
+//   </>
+// );
